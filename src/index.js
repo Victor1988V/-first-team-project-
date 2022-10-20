7aeb736462997729296fab14cd7d7a8999fd81da
Kulik Victor <kulic02041988@gmail.com>
2022-10-20 21:07:54 +0300

    copy

---
+++ src/index.js
@@ -0,0 +1,89 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    mobileMenu.classList.remove("is-open");
+  });
+}
+
+
+  // FAQ Accordion
+
+  const items = document.querySelectorAll('.faq__item');
+  let isTriggerd = false;
+
+  document.addEventListener('mouseover', function (evt) {
+    isTriggerd = true;
+    let target = evt.target;
+    let item = event.target.closest('.faq__item');
+    if (!item) return;
+    item.classList.remove('hide');
+  });
+
+  document.addEventListener('mouseout', function (evt) {
+    let target = evt.target;
+    let item = event.target.closest('.faq__item');
+    if (!item) return;
+    item.classList.add('hide');
+  });
+
+  document.addEventListener('click', function (evt) {
+    if (isTriggerd)
+    {
+      isTriggerd = false;
+      return;
+    }
+    let target = evt.target;
+    let item = event.target.closest('.faq__item');
+    if (!item) return;
+    item.classList.toggle('hide');
+  });
+
+})();
+
+//review
+
+var swiper = new Swiper(".reviews__slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + "0" + (index + 1) + "</span>";
+    },
+  },
+});
